ff07779a61fd5ecab9a8e4e638f0126fdf3a4f82
robo <palr74502@gmail.com>
2022-10-14 02:07:30 +0530

    js fun add

---
+++ index.js
@@ -0,0 +1,12 @@
+
+  
+// Function definition
+function welcomeMsg(name) {
+   document.write("Hello " + name + " welcome to GeeksforGeeks");
+}
+  
+// creating a variable
+var nameVal = "ROBO";
+  
+// calling the function
+welcomeMsg(nameVal);
